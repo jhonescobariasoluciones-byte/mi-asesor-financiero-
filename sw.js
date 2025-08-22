2ca307fbca8c433fe9f177b861ea20d5202d3f70
jhonescobariasoluciones-byte <jhonescobar.iasoluciones@gmail.com>
2025-08-22 13:22:21 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'mi-plan-financiero-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Aquí podrías añadir más archivos si los tuvieras (CSS, otras imágenes, etc.)
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
